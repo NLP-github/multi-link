ed51174a2a41b58287761076afc9b15f7ab59311
NLP-github <naumpopov@yahoo.de>
2023-11-20 11:04:02 +0300

    add

---
+++ README.md
@@ -0,0 +1,35 @@
+#Мульти-ссылочная страница
+
+Аналог таких сервисов как: **linktr**, **milkshake**, **Taplink** и многих других.
+
+## Технологии
+![ChakraUI](https://img.shields.io/badge/-chakraUI-61daf8?logo=chakraUI&logoColor=black)
+![React](https://img.shields.io/badge/-React-61daf8?logo=react&logoColor=black)
+![HTML5](https://img.shields.io/badge/-HTML5-e34f26?logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/-CSS3-1572b6?logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/-JavaScript-f7df1e?logo=javaScript&logoColor=black)
+![Webpack](https://img.shields.io/badge/-Webpack-99d6f8?logo=webpack&logoColor=black)
+
+
+Проект написан с использованием библиотеки компонентов для **React** - **Chakra UI**. [Ссылка на документацию Chakra UI.](https://chakra-ui.com/getting-started)
+
+## Запуск проекта:
+* Склонировать проект на ваш компьютер с Github с помощью команды:
+```
+git clone https://github.com/NLP-github/multi-link
+```
+
+* Установить зависимости с помощью команды:
+```
+npm install
+```
+* Запустить проект с помощью команды:
+```
+npm run start
+```
+
+## Настройка проекта
+
+* Для отображения иконок в проекте используется библиотека **React Icons**. [Ссылка на документацию **React Icons**.](https://react-icons.github.io/react-icons)
+
+* Анимации сделаны с использованием библиотеки: Motion. [Ссылка на документацию](https://www.framer.com/motion/introduction/)
---
+++ src/App.js
@@ -0,0 +1,345 @@
+import React, {useState, useEffect} from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  VStack,
+  Grid,
+  theme,
+  Image,
+  List,
+  HStack,
+  Skeleton, 
+  SkeletonCircle, 
+  SkeletonText,
+  ButtonGroup,
+  IconButton
+} from '@chakra-ui/react';
+import { Link as ChakraLink } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+//Подключение анимаций
+import { motion } from 'framer-motion';
+
+//Подключение иконок
+import {AiFillGithub, 
+  AiFillCheckCircle, 
+  AiFillYoutube, 
+  AiFillDollarCircle,
+  AiFillInstagram,
+} from 'react-icons/ai';
+
+import {FaTelegram, 
+  FaLinkedin, 
+  FaVk, 
+  FaGlobe,
+  FaYoutube,
+} from "react-icons/fa"
+
+function App() {
+
+  //Ссылки на социальные сети и их логотипы
+  const links = [
+    {url: "https://www.youtube.com", icon: AiFillYoutube, text: "YouTube"},
+    {url: "https://ya.ru/", icon: FaGlobe, text: "Мой Сайт"},
+    {url: "https://github.com/NLP-github", icon: AiFillGithub, text: "Мой GitHub"},
+    {url: "https://t.me/N_LP0", icon: FaTelegram, text: "Группа Telegram"},
+    {url: "https://boosty.to", icon: AiFillDollarCircle, text: "Поддержать канал"},
+    {url: "https://dzen.ru", icon: FaGlobe, text: "ДЗЕН"},
+    {url: "https://rutube.ru", icon: FaGlobe, text: "Rutube"},
+    {url: "https://www.tiktok.com", icon: FaGlobe, text: "TikTok"},
+  ]
+
+    //Ссылки на доп. материалы
+    const devlinks = [
+      {url: "https://github.com/", icon: AiFillGithub, text: "Github"},
+      {url: "https://github.com/", icon: AiFillGithub, text: "Github"},
+      {url: "https://", icon: FaYoutube, text: "Настрой свой ..."},
+      {url: "https://", icon: FaYoutube, text: "Telegram бот с GPT"},
+    ]
+
+    //Название страницы
+    const info = {
+      name: "@NLP",
+    }
+
+    // Настройка цвета страницы
+    const color = {
+      colorTheme: "linear-gradient(135deg, #e55d87 0%, #5fc3e4 100%)",
+      colorButton: "rgba(255,255,255)"
+    }
+
+    // Ссылки на личные социальные сети
+    const socialLink = {
+      vk:"https://vk.com/",
+      telegram:"https://t.me/",
+      instagram:"https://www.instagram.com/",
+      linkedin:"https://www.linkedin.com/",
+    }
+
+  const parentContainer = {
+    hidden: {opacity: 0},
+    show: {opacity: 1, transition: {staggerChildren: 0.15}},
+  }
+
+  const chieldElement = {
+    hidden: {opacity: 0, x: "-50px"},
+    show: {opacity: 1, x: "0px"}
+  }
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/');
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+        setIsLoading(false);
+      } else {
+        throw new Error('Ошибка получения данных');
+      }
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid 
+          minH="100vh"
+          p={3}
+          sx={{
+            backgroundImage: "url('./background.png')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundAttachment: "fixed",
+            backgroundBlendMode:"soft-light"
+          }}
+          >
+          <VStack marginTop="2em">
+
+            {/* логотип */}
+            <Link to="/" style={{ textDecoration: 'none' }}>
+            {isLoading ? (
+                <SkeletonCircle size="6em" />
+              ) : (
+                <Image 
+                  src="./Logo.png"
+                  alt="Логотип"
+                  w="9em"
+                  borderRadius="50%"
+                  border="3px solid white"
+                  boxShadow="0px 0px 30px rgba(0,0,0,0.5)"
+                  as={motion.img}
+                  initial={{scale: 0}}
+                  animate={{scale: 1}}
+                  transition="linear 0.1s"
+                  whileHover={{scale: 1.1}}
+                  whileTap={{scale: 0.9}}
+                />
+              )}
+            </Link>
+
+            {/* Блок с названием */}
+            <HStack
+              as={motion.div}
+              initial={{x: "-50px"}}
+              animate={{x: "0px"}}
+              transition="linear 0.1s"
+            >
+              {isLoading ? (
+                <>
+                  <SkeletonText noOfLines={1} width="10em" skeletonHeight='10'/>
+                  <Skeleton height="30px" width="30px" borderRadius="50%" />
+                </>
+              ) : (
+                <>
+                  <Text
+                    fontSize={30} 
+                    fontWeight="bold"
+                    color="white"
+                  >
+                    {info.name}
+                  </Text>
+                  <Text 
+                    as={AiFillCheckCircle} 
+                    color="rgba(3, 177, 252)" 
+                    fontSize={30} 
+                    marginRight="auto"
+                  />
+                </>
+              )}
+            </HStack>
+
+            {/* Блок с ссылками на мои соц сети */}
+            <ButtonGroup variant='solid' spacing='3' size='lg'>
+              {isLoading ? (
+                <>
+                  <Skeleton width="12" height="12" />
+                  <Skeleton width="12" height="12" />
+                  <Skeleton width="12" height="12" />
+                  <Skeleton width="12" height="12" />
+                </>
+              ) : (
+                <>
+                  <ChakraLink href={socialLink.vk} target="_blank">
+                    <IconButton
+                      bgColor={color.colorButton}
+                      color="gray.900"
+                      icon={<FaVk />}
+                      as={motion.div}
+                      whileHover={{scale: 1.1}}
+                      whileTap={{scale: 0.9}}
+                      _hover={{ // добавляем стили для hover состояния
+                        boxShadow:"3px 3px 3px 0px rgba(188,19,254)",
+                        color:"rgba(188,19,254)"
+                      }}
+                    />
+                  </ChakraLink>
+                  <ChakraLink href={socialLink.telegram} target="_blank">
+                    <IconButton
+                      bgColor={color.colorButton}
+                      color="gray.900"
+                      icon={<FaTelegram />}
+                      as={motion.div}
+                      whileHover={{scale: 1.1}}
+                      whileTap={{scale: 0.9}}
+                      _hover={{ // добавляем стили для hover состояния
+                        boxShadow:"3px 3px 3px 0px rgba(188,19,254)",
+                        color:"rgba(188,19,254)"
+                      }}
+                    />
+                  </ChakraLink>
+                  <ChakraLink href={socialLink.instagram} target="_blank">
+                    <IconButton
+                      bgColor={color.colorButton}
+                      color="gray.900"
+                      icon={<AiFillInstagram />}
+                      as={motion.div}
+                      whileHover={{scale: 1.1}}
+                      whileTap={{scale: 0.9}}
+                      _hover={{ // добавляем стили для hover состояния
+                        boxShadow:"3px 3px 3px 0px rgba(188,19,254)",
+                        color:"rgba(188,19,254)"
+                      }}
+                    />
+                  </ChakraLink>
+                  <ChakraLink href={socialLink.linkedin} target="_blank">
+                    <IconButton
+                      bgColor={color.colorButton}
+                      color="gray.900"
+                      icon={<FaLinkedin />}
+                      as={motion.div}
+                      whileHover={{scale: 1.1}}
+                      whileTap={{scale: 0.9}}
+                      _hover={{ // добавляем стили для hover состояния
+                        boxShadow:"3px 3px 3px 0px rgba(188,19,254)",
+                        color:"rgba(188,19,254)"
+                      }}
+                    />
+                  </ChakraLink>
+                </>
+              )}
+            </ButtonGroup>
+
+
+            {/* Блок с ссылками */}
+            <List as={motion.ul} variants={parentContainer} initial="hidden" animate="show">
+              {links.map(link => (
+                <a href={link.url}>
+                  <HStack 
+                    w="15em" 
+                    h="3em" 
+                    borderRadius="12px"
+                    boxShadow="10px 5px 5px rgba(0,0,0,0.5)"
+                    bgColor={color.colorButton}
+                    p="1em"
+                    marginY="1em"
+                    color="gray.900"
+                    as={motion.div}
+                    variants={chieldElement}
+                    whileHover={{scale: 1.1}}
+                    whileTap={{scale: 0.9}}
+                    _hover={{ // добавляем стили для hover состояния
+                      boxShadow:"12px 14px 14px 0px rgba(188,19,254)",
+                      color:"rgba(188,19,254)"
+                    }}
+                  >
+                    <Text 
+                      as={link.icon} 
+                      
+                      fontSize={30} 
+                      marginRight="auto"
+                    />
+                    <Text 
+                      
+                      fontSize={20} 
+                      fontWeight="bold"
+                      marginRight="auto"
+                      p="1em"
+                      marginY="1em"
+                    >
+                        {link.text}
+                    </Text>
+                  </HStack>
+                </a>
+              ))}
+            </List>
+
+            {/* Блок с доп ссылками */}
+            <List as={motion.ul} variants={parentContainer} initial="hidden" animate="show">
+              {devlinks.map(link => (
+                <a href={link.url}>
+                  <HStack 
+                    w="15em" 
+                    h="3em" 
+                    borderRadius="12px"
+                    boxShadow="10px 5px 5px rgba(0,0,0,0.5)"
+                    color="gray.900"
+                    bgColor={color.colorButton}
+                    p="1em"
+                    marginY="1em"
+                    _hover={{ // добавляем стили для hover состояния
+                      boxShadow:"12px 14px 14px 0px rgba(156, 38, 184, 0.84)",
+                      color:"rgba(156, 38, 184, 0.84)"
+                    }}
+                    as={motion.div}
+                    variants={chieldElement}
+                    whileHover={{scale: 1.1}}
+                    whileTap={{scale: 0.9}}
+                  >
+                    <Text 
+                      as={link.icon} 
+                      fontSize={30} 
+                      marginRight="auto"
+                    />
+                    <Text
+                      fontSize={20} 
+                      fontWeight="bold"
+                      marginRight="auto"
+                      p="1em"
+                      marginY="1em"
+                    >
+                        {link.text}
+                    </Text>
+                  </HStack>
+                </a>
+              ))}
+            </List>
+
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
